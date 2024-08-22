d252c4e88b897786be7ebd1d8f665b51d797887f
RenatoRissato <164811008+RenatoRissato@users.noreply.github.com>
2024-08-21 21:51:01 -0300

    Add files via upload

---
+++ doublebb.java
@@ -0,0 +1,12 @@
+public class doublebb
+{
+    public static void main(String args[])
+    {
+        double x;
+        
+        x = Math.max(5.9, 3.1);
+        
+        System.out.println("Resultado: " + x);
+
+    }
+}
