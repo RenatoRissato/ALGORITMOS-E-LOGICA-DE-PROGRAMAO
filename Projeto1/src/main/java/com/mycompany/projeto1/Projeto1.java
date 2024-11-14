778ad8e4bf05939eed8cc2872d701a6f9c417093
RenatoRissato <164811008+RenatoRissato@users.noreply.github.com>
2024-11-13 21:54:48 -0300

    Add files via upload

---
+++ Projeto1/src/main/java/com/mycompany/projeto1/Projeto1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.projeto1;
+
+/**
+ *
+ * @author .Aluno
+ */
+public class Projeto1 {
+
+    public static void main(String[] args) {
+        Principal formPrincipal = new Principal();
+        formPrincipal.setVisible(true);
+    }
+}
